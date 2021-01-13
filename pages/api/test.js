17e1ab2df7a259f46dd16978bb1910c920eed8eb
fahadsaleem1 <igz.iwd.25@gmail.com>
2021-01-13 17:00:57 +0500

    add

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,9 +1,20 @@
 var fs = require('fs')
+const path = './mynewfile2.txt'
 export default function handler(req, res) {
-    fs.writeFile('mynewfile2.txt', 'This is my text updated files added', function (err) {
-        if (err) throw err;
-        res.status(200).json({ text: 'Hello' })
-      });
+
+    if (fs.existsSync(path)) {
+        res.status(200).json({ text: 'file exist' })
+    } else {
+        res.status(200).json({ text: 'file not exist' })
+    }
+   
+
+
+
+    // fs.writeFile('mynewfile2.txt', 'This is my text updated files added s', function (err) {
+    //     if (err) throw err;
+    //     res.status(200).json({ text: 'Hello' })
+    //   });
         
    
   }
