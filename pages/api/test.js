d58f8cfb606281a23d0a99208ed299f7d53513da
fahadsaleem1 <igz.iwd.25@gmail.com>
2021-01-13 15:41:00 +0500

    add

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,6 +1,6 @@
 var fs = require('fs')
 export default function handler(req, res) {
-    fs.writeFile('mynewfile2.txt', 'This is my text', function (err) {
+    fs.writeFile('mynewfile2.txt', 'This is my text updated', function (err) {
         if (err) throw err;
         res.status(200).json({ text: 'Hello' })
       });
