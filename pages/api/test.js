6bd2528963688e7587f186bb03c86d5499d013c3
fahadsaleem1 <igz.iwd.25@gmail.com>
2021-01-13 15:00:04 +0500

    add

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,3 +1,7 @@
+var fs = require('fs')
 export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello' })
+    fs.open('mynewfile2.txt', 'w', function (err, file) {
+        if (err) throw err;
+        res.status(200).json({ text: 'Hello' })
+      });
   }
